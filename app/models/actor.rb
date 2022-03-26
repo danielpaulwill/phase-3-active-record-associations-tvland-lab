c741632af74b6e17ceae8fcc5081251dce89c644
Daniel Williams <danielpaulwill@gmail.com>
2022-03-26 13:46:00 -0400

    commit tree 1cbbadd1f2a1b47432d4879a912727cad82b6259

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,17 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
+
+  def list_roles
+    roles_list = []
+    self.characters.each do |character|
+      roles_list << character.name + " - " + character.show.name
+    end
+    roles_list
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,14 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    list_of_actors = []
+    actors.each do |actor|
+      list_of_actors << actor.first_name + " " + actor.last_name
+    end
+    list_of_actors
+  end
+
 end
